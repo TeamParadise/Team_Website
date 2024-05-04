8e373d8f8d698bbd9f5939e4854d5b91fecdd65a
John Studnicka <contact@zentec.dev>
2024-05-04 02:19:57 -0700

    Create .editorconfig and apply uniform code style

--- Pages/email.js
+++ Pages/email.js
@@ -1,46 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fields.firstName = document.getElementById("firstName");
-  fields.lastName = document.getElementById("lastName");
-  fields.email = document.getElementById("email");
-  fields.message = document.getElementById("question");
+    fields.firstName = document.getElementById("firstName");
+    fields.lastName = document.getElementById("lastName");
+    fields.email = document.getElementById("email");
+    fields.message = document.getElementById("question");
 });
 
 function isNotEmpty(value) {
-  if (value == null || typeof calue == "undefined") return false;
+    if (value == null || typeof calue == "undefined") return false;
 
-  return value.length > 0;
+    return value.length > 0;
 }
 
 function isNumber(num) {
-  return num.length > 0 && !isNaN(num);
+    return num.length > 0 && !isNaN(num);
 }
 
 function isEmail(email) {
-  let regex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  return regex.test(String(email).toLowerCase());
+    let regex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
 }
 
 //field.className to change classes
 
 class User {
-  constructor(firstName, lastName, email, question) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.question = question;
-  }
+    constructor(firstName, lastName, email, question) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.question = question;
+    }
 }
 
 function sendContact() {
-  if (isValid()) {
-    let usr = new User(
-      firstName.value,
-      lastName.value,
-      email.value,
-      question.value
-    );
-  }
+    if (isValid()) {
+        let usr = new User(
+            firstName.value,
+            lastName.value,
+            email.value,
+            question.value
+        );
+    }
 }
 
 /*function scrollPosition() {
